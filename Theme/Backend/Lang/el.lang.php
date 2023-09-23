c8eb656be55dbf08f7d0aa608a2bb528c37b8d49
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-23 23:05:18 +0000

    fix language

---
+++ Theme/Backend/Lang/el.lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\Localization
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+return ['BusinessExpenses' => [
+    ':status1'    => '',
+    ':status2'    => '',
+    ':status3'    => '',
+    'Amount'      => '',
+    'Approved'    => '',
+    'CreatedAt'   => '',
+    'Description' => '',
+    'End'         => '',
+    'Expenses'    => '',
+    'From'        => '',
+    'Gross'       => '',
+    'Net'         => '',
+    'Notes'       => '',
+    'Paid'        => '',
+    'Report'      => '',
+    'Start'       => '',
+    'Status'      => '',
+    'Title'       => '',
+]];
