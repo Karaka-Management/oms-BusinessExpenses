0152153bd3a40fca4c85f2de24f735cb82b19df6
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:38 +0000

    Add test cases for null models

--- tests/Models/NullExpenseElementTest.php
+++ tests/Models/NullExpenseElementTest.php
@@ -23,7 +23,7 @@ final class NullExpenseElementTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\BusinessExpenses\Models\NullExpenseElement
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullExpenseElementTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\BusinessExpenses\Models\NullExpenseElement
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
--- tests/Models/NullExpenseTest.php
+++ tests/Models/NullExpenseTest.php
@@ -23,7 +23,7 @@ final class NullExpenseTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\BusinessExpenses\Models\NullExpense
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullExpenseTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\BusinessExpenses\Models\NullExpense
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
