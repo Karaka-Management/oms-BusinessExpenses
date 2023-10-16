d6d5132e91412b284db5ec8c7634b34f998cdaa0
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 22:23:57 +0000

    test fixes

---
+++ tests/Models/NullExpenseElementTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\BusinessExpenses\tests\Models;
+
+use Modules\BusinessExpenses\Models\NullExpenseElement;
+
+/**
+ * @internal
+ */
+final class NullExpenseElementTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\BusinessExpenses\Models\NullExpenseElement
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\BusinessExpenses\Models\ExpenseElement', new NullExpenseElement());
+    }
+
+    /**
+     * @covers Modules\BusinessExpenses\Models\NullExpenseElement
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullExpenseElement(2);
+        self::assertEquals(2, $null->id);
+    }
+
+    /**
+     * @covers Modules\BusinessExpenses\Models\NullExpenseElement
+     * @group module
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullExpenseElement(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
---
+++ tests/Models/NullExpenseTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\BusinessExpenses\tests\Models;
+
+use Modules\BusinessExpenses\Models\NullExpense;
+
+/**
+ * @internal
+ */
+final class NullExpenseTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\BusinessExpenses\Models\NullExpense
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\BusinessExpenses\Models\Expense', new NullExpense());
+    }
+
+    /**
+     * @covers Modules\BusinessExpenses\Models\NullExpense
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullExpense(2);
+        self::assertEquals(2, $null->id);
+    }
+
+    /**
+     * @covers Modules\BusinessExpenses\Models\NullExpense
+     * @group module
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullExpense(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
