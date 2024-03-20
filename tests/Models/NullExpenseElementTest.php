9a79563cc0bb2d4de041cc784afa16bed6e54462
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-20 03:00:24 +0000

    fix tests

--- tests/Models/NullExpenseElementTest.php
+++ tests/Models/NullExpenseElementTest.php
@@ -22,7 +22,7 @@ use Modules\BusinessExpenses\Models\NullExpenseElement;
 final class NullExpenseElementTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @covers Modules\BusinessExpenses\Models\NullExpenseElement
+     * @covers \Modules\BusinessExpenses\Models\NullExpenseElement
      * @group module
      */
     public function testNull() : void
@@ -31,7 +31,7 @@ final class NullExpenseElementTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @covers Modules\BusinessExpenses\Models\NullExpenseElement
+     * @covers \Modules\BusinessExpenses\Models\NullExpenseElement
      * @group module
      */
     public function testId() : void
@@ -41,7 +41,7 @@ final class NullExpenseElementTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @covers Modules\BusinessExpenses\Models\NullExpenseElement
+     * @covers \Modules\BusinessExpenses\Models\NullExpenseElement
      * @group module
      */
     public function testJsonSerialize() : void
